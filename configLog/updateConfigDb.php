1a6f9d975bbf518f62296bd1adeb689a7b1d0395
William Rosenberg <williamrosenberg93@gmail.com>
2016-04-05 14:14:11 +0000

    added configLog

---
+++ configLog/updateConfigDb.php
@@ -0,0 +1,36 @@
+<?php
+
+
+
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "ithaax_testdata";
+
+	// Create connection
+	$conn = new mysqli($servername, $username, $password, $dbname);
+
+	// Check connection
+	if ($conn->connect_error) {
+	    die("Connection failed: " . $conn->connect_error);
+	}
+
+	
+	
+	$selectedConfig = $_POST['configInput'];
+	$inputValue = $_POST['configs'];
+
+
+	$sql = "UPDATE configs SET $inputValue = $selectedConfig where id=1";
+
+	if ($conn->query($sql) === TRUE) {
+	    header('Location: http://localhost/Remote-sailing-robots/configLog/');
+	} else {
+	    echo "Error updating record: " . $conn->error;
+	}
+
+	$conn->close();
+
+
+
+?>
