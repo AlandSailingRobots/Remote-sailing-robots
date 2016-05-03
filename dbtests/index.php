534a1d28892bd6b597e5956083fa289a62c73e04
mbrandtb <mathias.brandtberg@ha.ax>
2016-05-03 12:35:29 +0300

    you can now go trough all the tables in logs

--- dbtests/index.php
+++ dbtests/index.php
@@ -48,11 +48,11 @@
         </div>
         <div id="navbar" class="navbar-collapse collapse">
           <ul class="nav navbar-nav navbar-right">
-            <li><a href="#">GpsData</a></li>
-            <li><a href="#">CourseData</a></li>
-            <li><a href="#">WindSensorData</a></li>
-            <li><a href="#">CompassData</a></li>
-            <li><a href="#">SystemDatalogs</a></li>
+            <li><a href="index.php">GpsData</a></li>
+            <li><a href="course.php">CourseData</a></li>
+            <li><a href="windsensor.php">WindSensorData</a></li>
+            <li><a href="compass.php">CompassData</a></li>
+            <li><a href="system.php">SystemDatalogs</a></li>
           </ul>
         </div>
       </div>
@@ -64,18 +64,22 @@
           <ul class="nav nav-sidebar">
             <?php
                 require('dbconnection.php');
-              //  echo "<li ><a href=\"?page=$gpsData\">GpsData </a></li>";
+                $result = getData("gps_dataLogs");
+                $pages = getPages("gps_dataLogs");
+                $number = getNumber();
+                $next = getNext();
+                $prev = getPrev();
               ?>
 
-            <li ><a href="#">GpsData </a></li>
-            <li><a href="#">CourseData</a></li>
-            <li><a href="#">WindSensorData</a></li>
-            <li><a href="#">CompassData</a></li>
-            <li><a href="#">SystemDatalogs</a></li>
+            <li ><a href="index.php">GpsData </a></li>
+            <li><a href="course.php">CourseData</a></li>
+            <li><a href="windsensor.php">WindSensorData</a></li>
+            <li><a href="compass.php">CompassData</a></li>
+            <li><a href="system.php">SystemDatalogs</a></li>
           </ul>
         </div>
         <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-          <h2 class="sub-header">Log Table</h2>
+          <h2 class="sub-header">Gps Logs</h2>
           <div class="table-responsive">
             <table class="table table-striped">
               <?php
@@ -101,14 +105,7 @@
               ?>
               <thead>
                 <tr>
-                  <th>id_system</th>
-                  <th>boat_id</th>
-                  <th>sail_command_sail</th>
-                  <th>rudder_command_rudder</th>
-                  <th>sail_servo_position</th>
-                  <th>rudder_servo_position</th>
-                  <th>waypoint_id</th>
-                  <th>true_wind_direction_calc</th>
+
                   <th>id_gps</th>
                   <th>time</th>
                   <th>latitude</th>
@@ -116,38 +113,17 @@
                   <th>heading</th>
                   <th>satellites_used</th>
                   <th>longitude</th>
-                  <th>id_course_calculation</th>
-                  <th>distance_to_waypoint</th>
-                  <th>bearing_to_waypoint</th>
-                  <th>course_to_steer</th>
-                  <th>tack</th>
-                  <th>going_starboard</th>
-                  <th>id_windsensor</th>
-                  <th>direction</th>
-                  <th>speed</th>
-                  <th>temperature</th>
-                  <th>id_compass_model</th>
-                  <th>heading</th>
-                  <th>pitch</th>
-                  <th>roll</th>
+
                 </tr>
               </thead>
 
               <tbody>
                 <?php
-
                 foreach($result as $key => $row)
                 {
                   echo "
                     <tr>
-                      <td>".$row["id_system"]."</td>
-                      <td>".$row["boat_id"]."</td>
-                      <td>".$row["sail_command_sail"]."</td>
-                      <td>".$row["rudder_command_rudder"]."</td>
-                      <td>".$row["sail_servo_position"]."</td>
-                      <td>".$row["rudder_servo_position"]."</td>
-                      <td>".$row["waypoint_id"]."</td>
-                      <td>".$row["true_wind_direction_calc"]."</td>
+
                       <td>".$row["id_gps"]."</td>
                       <td>".$row["time"]."</td>
                       <td>".$row["latitude"]."</td>
@@ -155,22 +131,7 @@
                       <td>".$row["heading"]."</td>
                       <td>".$row["satellites_used"]."</td>
                       <td>".$row["longitude"]."</td>
-                      <td>".$row["id_course_calculation"]."</td>
-                      <td>".$row["distance_to_waypoint"]."</td>
-                      <td>".$row["bearing_to_waypoint"]."</td>
-                      <td>".$row["course_to_steer"]."</td>
-                      <td>".$row["tack"]."</td>
-                      <td>".$row["going_starboard"]."</td>
-                      <td>".$row["id_windsensor"]."</td>
-                      <td>".$row["direction"]."</td>
-                      <td>".$row["speed"]."</td>
-                      <td>".$row["temperature"]."</td>
-                      <td>".$row["id_compass_model"]."</td>
-                      <td>".$row["heading"]."</td>
-                      <td>".$row["pitch"]."</td>
-                      <td>".$row["roll"]."</td>
-
-                  ";
+                      ";
                 }
                 ?>
               </tbody>
