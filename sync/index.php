f5948fb53d835687a6759bad1d8156aebad926a0
William Rosenberg <william.rosenberg@ha.ax>
2016-05-20 09:05:45 +0300

    restructure of sync page

--- sync/index.php
+++ sync/index.php
@@ -1,97 +1,49 @@
 <?php
-	/*
-	* PHP SOAP - How to create a SOAP Server and a SOAP Client
-	*/
-	if(isset($_GET["id"]) && isset($_GET["pwd"])) {
-
-
-		 //$options = array('location' => 'http://www.sailingrobots.com/testdata/sync/server.php', 'uri' => 'http://localhost/');
-
-		// $options = array('location' => 'http://www.sailingrobots.com/testdata/sync/server.php', 'uri' => 'http://localhost/');
-
-
-		 //$options = array('location' => 'http://www.sailingrobots.com/testdata/sync/server.php', 'uri' => 'http://localhost/');
-		//this is for local usage of httpSync
-		$options = array('location' => 'http://localhost/Remote-sailing-robots/sync/server.php', 'uri' => 'http://localhost/');
+	if(isset($_POST["id"]) && isset($_POST["pwd"])) {
+		$optionsPushlogs = array('location' => 'http://localhost/Remote-sailing-robots/sync/pushDatalogs.php', 'uri' => 'http://localhost/');
+		 //$options = array('location' => 'http://www.sailingrobots.com/testdata/sync/pushDatalogs.php', 'uri' => 'http://localhost/');
+		$optionsGetConfigs = array('location' => 'http://localhost/Remote-sailing-robots/sync/getConfigs.php', 'uri' => 'http://localhost/');
+		//$options = array('location' => 'http://www.sailingrobots.com/testdata/sync/getConfigs.php', 'uri' => 'http://localhost/');
+		$optionsPushConfigs = array('location' => 'http://localhost/Remote-sailing-robots/sync/pushConfigs.php', 'uri' => 'http://localhost/');
+		//$options = array('location' => 'http://www.sailingrobots.com/testdata/sync/pushConfigs.php, 'uri' => 'http://localhost/');
+		$optionsPushwaypoints = array('location' => 'http://localhost/Remote-sailing-robots/sync/pushWaypoints.php', 'uri' => 'http://localhost/');
+		//$options = array('location' => 'http://www.sailingrobots.com/testdata/sync/pushWaypoints.php, 'uri' => 'http://localhost/');
+		
 		//create an instante of the SOAPClient (the API will be available)
-		$service = new SoapClient(NULL, $options);
-		//call an API method
+		$pushLogsService = new SoapClient(NULL, $optionsPushlogs);
+		$getConfigsService = new SoapClient(NULL, $optionsGetConfigs);
+		$pushConfigsService = new SoapClient(NULL, $optionsPushConfigs);
+		$pushPushWaypoints = new SoapClient(NULL, $optionsPushwaypoints);
 
-		/*if(!($service->getUser($_GET["id"], $_GET["pwd"]) === 1)) {
-			return;
-		}*/
-
-		if(isset($_GET["serv"])) {
+		if(isset($_POST["serv"])) {
 			try {
-				switch($_GET["serv"]) {
-					case "getSetup":
-						print_r($service->getSetup($_GET["id"]));
-						break;
-					case "getConfig":
-						print_r($service->getConfig($_GET["id"]));
-						break;
-
-					case "getAllConfigs":
-						print_r($service->getAllConfigs($_GET["id"]));
-						break;
-					case "getRoute":
-						print_r($service->getRoute($_GET["id"]));
-						break;
-					case "getCourseCalculationConfig":
-						print_r($service->getCourseCalculationConfig($_GET["id"]));
-						break;
-					case "getMaestroControllerConfig":
-						print_r($service->getMaestroControllerConfig($_GET["id"]));
-						break;
-					case "getRudderCommandConfig":
-						print_r($service->getRudderCommandConfig($_GET["id"]));
-						break;
-					case "getRudderServoConfig":
-						print_r($service->getRudderServoConfig($_GET["id"]));
-						break;
-					case "getSailingRobotConfig":
-						print_r($service->getSailingRobotConfig($_GET["id"]));
-						break;
-					case "getSailCommandConfig":
-						print_r($service->getSailCommandConfig($_GET["id"]));
-						break;
-					case "getSailServoConfig":
-						print_r($service->getSailServoConfig($_GET["id"]));
-						break;
-					case "getWaypointRoutingConfig":
-						print_r($service->getWaypointRoutingConfig($_GET["id"]));
-						break;
-					case "getWindSensorConfig":
-						print_r($service->getWindSensorConfig($_GET["id"]));
-						break;
-					case "getWindVaneConfig":
-						print_r($service->getWindVaneConfig($_GET["id"]));
-						break;
-
+				switch($_POST["serv"]) {
 					case "checkIfNewConfigs":
-						echo $service->checkIfNewConfigs();
+						echo $getConfigsService->checkIfNewConfigs();
 						break;
 					case "setConfigsUpdated":
-						print_r($service->setConfigsUpdated());
+						print_r($getConfigsService->setConfigsUpdated());
+						break;
+					case "getAllConfigs":
+						print_r($getConfigsService->getAllConfigs($_POST["id"]));
+						break;
+					case "pushConfigs":
+						print_r($pushConfigsService->pushConfigs($_POST["data"]));
 						break;
 					case "pushWaypoints":
-						print_r($service->pushWaypoint($_GET["data"]));
+						print_r($pushPushWaypoints->pushWaypoint($_POST["data"]));
 						break;
 					case "pushAllLogs":
-						print_r($service->pushAllLogs($_GET["id"], $_GET["data"]));
-						break;
-					case "pushLogs":
-						print_r($service->pushLogs($_GET["id"], $_GET["data"]));
+						print_r($pushLogsService->pushAllLogs($_POST["id"], $_POST["data"]));
 						break;
 					default:
 						break;
 				}
 			} catch(Exception $e) {
 			print_r("ERROR: ".$e->getMessage());
-
 			}
 		}
 	} else {
-		echo "CONNY";
+		echo "Does not work";
 	}
 ?>
