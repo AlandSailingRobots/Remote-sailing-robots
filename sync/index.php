49586e69d620e231ac8b788f76d5ac41d06bf343
mbrandtb <mathias.brandtberg@ha.ax>
2016-05-20 09:59:50 +0300

    fixed merging errors in sync

--- sync/index.php
+++ sync/index.php
@@ -1,15 +1,4 @@
 <?php
-<<<<<<< HEAD
-	/*
-	* PHP SOAP - How to create a SOAP Server and a SOAP Client
-	*/
-	if(isset($_POST["id"]) && isset($_POST["pwd"])) {
-
-		 //$options = array('location' => 'http://www.sailingrobots.com/testdata/sync/server.php', 'uri' => 'http://localhost/');
-
-		 //$options = array('location' => 'http://www.sailingrobots.com/testdata/sync/server.php', 'uri' => 'http://localhost/');
-		//this is for local usage of httpSync
-		$options = array('location' => 'http://localhost/Remote-sailing-robots/sync/server.php', 'uri' => 'http://localhost/');
 	if(isset($_POST["id"]) && isset($_POST["pwd"])) {
 		$optionsPushlogs = array('location' => 'http://localhost/Remote-sailing-robots/sync/pushDatalogs.php', 'uri' => 'http://localhost/');
 		 //$options = array('location' => 'http://www.sailingrobots.com/testdata/sync/pushDatalogs.php', 'uri' => 'http://localhost/');
@@ -19,8 +8,7 @@
 		//$options = array('location' => 'http://www.sailingrobots.com/testdata/sync/pushConfigs.php, 'uri' => 'http://localhost/');
 		$optionsPushwaypoints = array('location' => 'http://localhost/Remote-sailing-robots/sync/pushWaypoints.php', 'uri' => 'http://localhost/');
 		//$options = array('location' => 'http://www.sailingrobots.com/testdata/sync/pushWaypoints.php, 'uri' => 'http://localhost/');
-
-
+		
 		//create an instante of the SOAPClient (the API will be available)
 		$pushLogsService = new SoapClient(NULL, $optionsPushlogs);
 		$getConfigsService = new SoapClient(NULL, $optionsGetConfigs);
@@ -28,16 +16,6 @@
 		$pushPushWaypoints = new SoapClient(NULL, $optionsPushwaypoints);
 
 		if(isset($_POST["serv"])) {
-
-			error_log($_POST["id"], 0);
-			error_log($_POST["data"], 0);
-			try {
-				switch($_POST["serv"]) {
-					case "getSetup":
-						print_r($service->getSetup($_POST["id"]));
-						break;
-					case "getConfig":
-						print_r($service->getConfig($_POST["id"]));
 			try {
 				switch($_POST["serv"]) {
 					case "checkIfNewConfigs":
@@ -47,57 +25,6 @@
 						print_r($getConfigsService->setConfigsUpdated());
 						break;
 					case "getAllConfigs":
-						print_r($service->getAllConfigs($_POST["id"]));
-						break;
-					case "getRoute":
-						print_r($service->getRoute($_POST["id"]));
-						break;
-					case "getCourseCalculationConfig":
-						print_r($service->getCourseCalculationConfig($_POST["id"]));
-						break;
-					case "getMaestroControllerConfig":
-						print_r($service->getMaestroControllerConfig($_POST["id"]));
-						break;
-					case "getRudderCommandConfig":
-						print_r($service->getRudderCommandConfig($_POST["id"]));
-						break;
-					case "getRudderServoConfig":
-						print_r($service->getRudderServoConfig($_POST["id"]));
-						break;
-					case "getSailingRobotConfig":
-						print_r($service->getSailingRobotConfig($_POST["id"]));
-						break;
-					case "getSailCommandConfig":
-						print_r($service->getSailCommandConfig($_POST["id"]));
-						break;
-					case "getSailServoConfig":
-						print_r($service->getSailServoConfig($_POST["id"]));
-						break;
-					case "getWaypointRoutingConfig":
-						print_r($service->getWaypointRoutingConfig($_POST["id"]));
-						break;
-					case "getWindSensorConfig":
-						print_r($service->getWindSensorConfig($_POST["id"]));
-						break;
-					case "getWindVaneConfig":
-						print_r($service->getWindVaneConfig($_POST["id"]));
-						break;
-
-					case "checkIfNewConfigs":
-						echo $service->checkIfNewConfigs();
-						break;
-					case "setConfigsUpdated":
-						print_r($service->setConfigsUpdated());
-						break;
-					case "pushWaypoints":
-						print_r($service->pushWaypoint($_POST["data"]));
-						break;
-					case "pushAllLogs":
-						print_r($service->pushAllLogs($_POST["id"], $_POST["data"]));
-
-						break;
-					case "pushLogs":
-						print_r($service->pushLogs($_POST["id"], $_POST["data"]));
 						print_r($getConfigsService->getAllConfigs($_POST["id"]));
 						break;
 					case "pushConfigs":
