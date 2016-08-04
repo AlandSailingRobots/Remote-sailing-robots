920107fa9a170492e0dce82294beb3d5cbdbb7a8
EMIL S <william.soderlund@ha.ax>
2016-08-04 09:30:37 +0300

    Fixed non-comment comment error

--- globalsettings.php
+++ globalsettings.php
@@ -1,17 +1,19 @@
-/****************************************************************************************
- *
- * Purpose:
- *      Provides global server settings (and potentially other things in the future).
- *      Make sure you include it in all pages that might need it (e.g. database handlers).
- *      The commented lines are for hostǵator so if you make any changes that you have to push
- *      up there, make sure you reverse the commenting.
- *
- * Developer Notes:
- *
- *
- ***************************************************************************************/
-
 <?php
+
+//***************************************************************************************
+//
+// Purpose:
+//     Provides global server settings (and potentially other things in the future).
+//    Make sure you include it in all pages that might need it (e.g. database handlers).
+//    The commented lines are for hostǵator so if you make any changes that you have to push
+//    up there, make sure you reverse the commenting.
+//
+// Developer Notes:
+//
+//
+//**************************************************************************************/
+
+
 
     $GLOBALS['server'] = "http://localhost/Remote-sailing-robots/";
     //$GLOBALS['server'] = "http://www.sailingrobots.com/testdata/";
